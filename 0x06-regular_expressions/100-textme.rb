a727e25598f84b6373d3ce375eb5620c2172ed0e
angellovc <vc.angel@outlook.com>
2020-07-08 15:06:48 +0000

    100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,8 +1,3 @@
 #!/usr/bin/env ruby
 
-
-from = ARGV[0].scan(/from:(.\w+)/).join
-to = ARGV[0].scan(/to:([\w\+-]+)/).join
-flags = ARGV[0].scan(/flags:([\w\+-:]+)/).join
-
-puts "#{from},#{to},#{flags}"
+puts ARGV[0].scan(/(?<=from:|to:|flags:)[\w\d+-:]+/).join(',')
