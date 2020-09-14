5a0b629f4394ffe64f8c47ad6352182d11da7a92
angellovc <vc.angel@outlook.com>
2020-09-14 21:32:26 +0000

    0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" returns information about his/her TODO list progress """
+from sys import argv
+import requests
+
+
+r = requests.get('https://jsonplaceholder.typicode.com/users')
+users = r.json()
+for user in users:
+    if user['id'] == int(argv[1]):
+        user_name = user['name']
+        user_id = user['id']
+        break
+
+r = requests.get('https://jsonplaceholder.typicode.com/todos')
+todos = r.json()
+
+total_tasks = 0
+completed = 0
+tasks = []
+
+for todo in todos:
+    if todo['userId'] == user_id:
+        total_tasks += 1
+        if todo['completed'] is True:
+            completed += 1
+            tasks.append(todo['title'])
+
+print("Employee {} is done with tasks({}/{}):".format(
+    user_name,
+    completed,
+    total_tasks)
+)
+for task in tasks:
+    print("    "+task)
