af998315b4e379187d6d487fcdf8eaa3a1b5d72c
angellovc <vc.angel@outlook.com>
2020-09-17 22:11:56 +0000

    2

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" recurse functions module """
+import requests
+
+
+def recurse(subreddit, hot_list=[], pag=-1):
+    """queries the Reddit API and returns a list of all hot titles
+    """
+    if pag == -1:
+        request = requests.get(
+            'https://www.reddit.com/r/{}/hot.json?show=all'.format(subreddit),
+            headers={'User-agent': 'custom'},
+            allow_redirects=False
+            )
+        if (request.status_code == 200):
+            posts = request.json()['data']['children']
+            recurse(posts, hot_list, pag + 1)
+        else:
+            return None
+    try:
+        subreddit[pag]
+        hot_list.append(subreddit[pag]['data']['title'])
+        hot_list = recurse(subreddit, hot_list, pag + 1)
+    except:
+        return hot_list
