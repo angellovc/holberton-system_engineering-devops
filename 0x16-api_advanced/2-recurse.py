a9dbf848e3c74982f0eff5d32bcbb5019e00a214
angellovc <vc.angel@outlook.com>
2020-09-17 22:47:57 +0000

    2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,7 +3,7 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[], pag=-1):
+def recurse(subreddit, hot_list=[]):
     """queries the Reddit API and returns a list of all hot titles
     """
     if isinstance(subreddit, str):
@@ -14,11 +14,12 @@ def recurse(subreddit, hot_list=[], pag=-1):
             )
         if (request.status_code == 200):
             posts = request.json()['data']['children']
-            recurse(posts, hot_list, pag + 1)
+            recurse(posts, hot_list)
         else:
             return None
     try:
+        pag = len(hot_list)
         hot_list.append(subreddit[pag]['data']['title'])
-        hot_list = recurse(subreddit, hot_list, pag + 1)
+        hot_list = recurse(subreddit, hot_list)
     except:
         return hot_list
