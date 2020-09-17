b46465262598bffa99eeb02f8cef61dc34759f71
angellovc <vc.angel@outlook.com>
2020-09-17 21:18:20 +0000

    1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" top_ten functions module """
+import requests
+from sys import argv
+import json
+
+
+def top_ten(subreddit):
+    """queries the Reddit API and prints the titles of the
+    first 10 hot posts listed for a given subreddit
+    """
+    request = requests.get(
+        'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit),
+        headers={'User-agent': 'custom'}
+    )
+    try:
+        posts = request.json()['data']['children']
+        for post in posts:
+            print(post['data']['title'])
+    except:
+        print(None)
